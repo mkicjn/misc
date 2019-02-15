5c2ae0d1c4f54cb6edc5c3c89d4906c5a32887f2
Armok628 <Mark.Johnson.62899@tamu.edu>
2019-02-15 00:34:33 -0600

    Tiny update

--- newpath.c
+++ newpath.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// TODO: Actually implement A*
+// TODO: Jump point search or best-first
+void dist(int p1,int p2,int w)
+{ // Chessboard distance
+	int dx=p2%w-p1%w;
+	int dy=p2/w-p1/w;
+	return dx>dy?dx:dy;
+}
 void randomize_map(char *m,int w,int h)
 {
 	int a=w*h;
