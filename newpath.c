0aab191ce0ae41db4ca7c0827256c342f18b0eab
Armok628 <Mark.Johnson.62899@tamu.edu>
2019-02-16 16:51:06 -0600

    Jump functions

--- newpath.c
+++ newpath.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// TODO: Jump point search or best-first
-void dist(int p1,int p2,int w)
+#include "jump.c"
+enum dir {
+	SOUTHWEST=1,SOUTH,SOUTHEAST,
+	WEST,NONE,EAST,
+	NORTHWEST,NORTH,NORTHEAST
+}; // Represents numpad
+int dist(int p1,int p2,int w)
 { // Chessboard distance
 	int dx=p2%w-p1%w;
 	int dy=p2/w-p1/w;
 	return dx>dy?dx:dy;
 }
+// Map generation
 void randomize_map(char *m,int w,int h)
 {
 	int a=w*h;
@@ -15,13 +21,17 @@ void randomize_map(char *m,int w,int h)
 		m[i]='#';
 	for (int x=1;x<w-1;x++)
 	for (int y=1;y<h-1;y++)
-		if (rand()%3)
+		if (rand()%4)
 			m[x+y*w]=' ';
 }
 void print_map(char *m,int w,int h)
 {
 	int a=w*h;
 	for (int i=0;i<a;i++) {
+		if (m[i]=='#')
+			printf("\033[1;30m");
+		else
+			printf("\033[1;31m");
 		putchar(m[i]);
 		if (i%w==w-1)
 			putchar('\n');
@@ -29,11 +39,25 @@ void print_map(char *m,int w,int h)
 }
 int main(int argc,char **argv)
 {
-	static const int WIDTH=80,HEIGHT=24;
+	static const int WIDTH=50,HEIGHT=25;
 	static const int AREA=WIDTH*HEIGHT;
 	static char map[AREA];
 	srand(time(NULL));
+
 	randomize_map(map,WIDTH,HEIGHT);
+	int s=rand()%AREA;
+	map[s]='5';
+	printf("Start: %d\n",s);
+	for (int i=2;i<=8;i+=2) {
+		int n=jump[i](map,WIDTH,HEIGHT,s);
+		if (n>=0)
+			map[n]='0'+i;
+	}
+	for (int i=1;i<=9;i+=2) {
+		int n=jump[i](map,WIDTH,HEIGHT,s);
+		if (n>=0)
+			map[n]='0'+i;
+	}
 	print_map(map,WIDTH,HEIGHT);
 	return 0;
 }
