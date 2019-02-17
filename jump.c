3907dcc3f5cc5da946a6a8bf34a99d8908becd42
Armok628 <Mark.Johnson.62899@tamu.edu>
2019-02-16 19:02:18 -0600

    Finish (?) jump point search

--- jump.c
+++ jump.c
@@ -1,124 +1,137 @@
-int jump_north(char *m,const int w,const int h,int n)
+// TODO: Refactor, for real
+int jump_north(char *m,const int w,const int h,int n,int g)
 {
 	n-=w;
 	while (n>0) {
-		if (m[n]=='#')
+		if (n==g)
+			return g;
+		if (m[n]!=' ')
 			return -1;
-		if (m[n+1]=='#'&&m[n+1-w]==' ')
+		if (m[n+1]=='#'&&m[n+1-w]!='#')
 			return n;
-		if (m[n-1]=='#'&&m[n-1-w]==' ')
+		if (m[n-1]=='#'&&m[n-1-w]!='#')
 			return n;
 		n-=w;
 	}
 	return -1;
 }
-int jump_south(char *m,const int w,const int h,int n)
+int jump_south(char *m,const int w,const int h,int n,int g)
 {
 	n+=w;
 	while (n<w*h) {
-		if (m[n]=='#')
+		if (n==g)
+			return g;
+		if (m[n]!=' ')
 			return -1;
-		if (m[n+1]=='#'&&m[n+1+w]==' ')
+		if (m[n+1]=='#'&&m[n+1+w]!='#')
 			return n;
-		if (m[n-1]=='#'&&m[n-1+w]==' ')
+		if (m[n-1]=='#'&&m[n-1+w]!='#')
 			return n;
 		n+=w;
 	}
 	return -1;
 }
-int jump_west(char *m,const int w,const int h,int n)
+int jump_west(char *m,const int w,const int h,int n,int g)
 {
 	n--;
 	while (n%w<w-1) {
-		if (m[n]=='#')
+		if (n==g)
+			return g;
+		if (m[n]!=' ')
 			return -1;
-		if (m[n+w]=='#'&&m[n+w-1]==' ')
+		if (m[n+w]=='#'&&m[n+w-1]!='#')
 			return n;
-		if (m[n-w]=='#'&&m[n-w-1]==' ')
+		if (m[n-w]=='#'&&m[n-w-1]!='#')
 			return n;
-		n++;
+		n--;
 	}
 	return -1;
 }
-int jump_east(char *m,const int w,const int h,int n)
+int jump_east(char *m,const int w,const int h,int n,int g)
 {
 	n++;
 	while (n%w>0) {
-		if (m[n]=='#')
+		if (n==g)
+			return g;
+		if (m[n]!=' ')
 			return -1;
-		if (m[n+w]=='#'&&m[n+w+1]==' ')
+		if (m[n+w]=='#'&&m[n+w+1]!='#')
 			return n;
-		if (m[n-w]=='#'&&m[n-w+1]==' ')
+		if (m[n-w]=='#'&&m[n-w+1]!='#')
 			return n;
 		n++;
 	}
 	return -1;
 }
-int jump_northeast(char *m,const int w,const int h,int n)
+int jump_northeast(char *m,const int w,const int h,int n,int g)
 {
 	n+=1-w;
 	while (n>0&&n%w>0) {
-		printf("Searching NE:%d\n",n);
-		if (m[n]=='#')
+		if (n==g)
+			return g;
+		if (m[n]!=' ')
 			return -1;
-		if (jump_north(m,w,h,n)>=0)
+		if (jump_north(m,w,h,n,g)>=0)
 			return n;
-		if (jump_east(m,w,h,n)>=0)
+		if (jump_east(m,w,h,n,g)>=0)
 			return n;
 		n+=1-w;
 	}
 	return -1;
 }
-int jump_northwest(char *m,const int w,const int h,int n)
+int jump_northwest(char *m,const int w,const int h,int n,int g)
 {
 	n+=-1-w;
 	while (n>0&&n%w<w-1) {
-		printf("Searching NW:%d\n",n);
-		if (m[n]=='#')
+		if (n==g)
+			return g;
+		if (m[n]!=' ')
 			return -1;
-		if (jump_north(m,w,h,n)>=0)
+		if (jump_north(m,w,h,n,g)>=0)
 			return n;
-		if (jump_west(m,w,h,n)>=0)
+		if (jump_west(m,w,h,n,g)>=0)
 			return n;
 		n+=-1-w;
 	}
 	return -1;
 }
-int jump_southeast(char *m,const int w,const int h,int n)
+int jump_southeast(char *m,const int w,const int h,int n,int g)
 {
 	n+=1+w;
 	while (n<w*h&&n%w>0) {
-		printf("Searching SE:%d\n",n);
-		if (m[n]=='#')
+		if (n==g)
+			return g;
+		if (m[n]!=' ')
 			return -1;
-		if (jump_south(m,w,h,n)>=0)
+		if (jump_south(m,w,h,n,g)>=0)
 			return n;
-		if (jump_east(m,w,h,n)>=0)
+		if (jump_east(m,w,h,n,g)>=0)
 			return n;
 		n+=1+w;
 	}
 	return -1;
 }
-int jump_southwest(char *m,const int w,const int h,int n)
+int jump_southwest(char *m,const int w,const int h,int n,int g)
 {
 	n+=w-1;
 	while (n<w*h&&n%w<w-1) {
-		printf("Searching SW:%d\n",n);
-		if (m[n]=='#')
+		if (n==g)
+			return g;
+		if (m[n]!=' ')
 			return -1;
-		if (jump_south(m,w,h,n)>=0)
+		if (jump_south(m,w,h,n,g)>=0)
 			return n;
-		if (jump_west(m,w,h,n)>=0)
+		if (jump_west(m,w,h,n,g)>=0)
 			return n;
 		n+=w-1;
 	}
 	return -1;
 }
-int no_jump(char *m,const int w,const int h,int n)
+int no_jump(char *m,const int w,const int h,int n,int g)
 {
 	return -1;
 }
-int (*jump[10])(char *,const int,const int,int)={
+int (*jump[10])(char *,const int,const int,int,int)={
 	&no_jump,
 	&jump_southwest,
 	&jump_south,
--- newpath.c
+++ newpath.c
@@ -8,10 +8,10 @@ enum dir {
 	NORTHWEST,NORTH,NORTHEAST
 }; // Represents numpad
 int dist(int p1,int p2,int w)
-{ // Chessboard distance
+{
 	int dx=p2%w-p1%w;
 	int dy=p2/w-p1/w;
-	return dx>dy?dx:dy;
+	return dx*dx+dy*dy;//dx>dy?dx:dy;
 }
 // Map generation
 void randomize_map(char *m,int w,int h)
@@ -21,21 +21,80 @@ void randomize_map(char *m,int w,int h)
 		m[i]='#';
 	for (int x=1;x<w-1;x++)
 	for (int y=1;y<h-1;y++)
-		if (rand()%4)
+		if (rand()%20)
 			m[x+y*w]=' ';
 }
 void print_map(char *m,int w,int h)
 {
 	int a=w*h;
 	for (int i=0;i<a;i++) {
-		if (m[i]=='#')
-			printf("\033[1;30m");
-		else
-			printf("\033[1;31m");
-		putchar(m[i]);
+		switch (m[i]) {
+		case '#':
+			printf("\033[1;30m#");
+			break;
+		case '.':
+			printf("\033[1;33m.");
+			break;
+		case 'O':
+			printf("\033[1;34mO");
+			break;
+		case 'X':
+			printf("\033[1;31mX");
+			break;
+		default:
+			printf("\033[m%c",m[i]);
+		}
 		if (i%w==w-1)
 			putchar('\n');
 	}
+}
+void swap(int *a,int *b)
+{
+	int c=*a;
+	*a=*b;
+	*b=c;
+}
+enum dir path(char *map,int w,int h,int start,int goal)
+{
+	int dirs[10],jps[10],dists[10];
+	for (int i=0;i<10;i++) {
+		dirs[i]=i;
+		jps[i]=jump[i](map,w,h,start,goal);
+		if (jps[i]>=0) {
+			if (jps[i]==goal)
+				return i;
+			dists[i]=dist(jps[i],goal,w);
+		} else
+			dists[i]=-1;
+	}
+	// Sort jump points by increasing distance to goal
+	for (int i=0;i<9;i++)
+		for (int j=i+1;j&&dists[j]<dists[j-1];j--) {
+			swap(&dists[j-1],&dists[j]);
+			swap(&dirs[j-1],&dirs[j]);
+			swap(&jps[j-1],&jps[j]);
+		}
+	// Print nodes
+	for (int i=0;i<9;i++) {
+		if (jps[i]>-1)
+			printf("Jump point from %d: %d (%d)\n",start,jps[i],dists[i]);
+	}
+	// Check each jump point for viable path
+	for (int i=0;i<9;i++) {
+		if (jps[i]<0)
+			continue;
+		if (dists[i]==0)
+			return dirs[i];
+		if (path(map,w,h,jps[i],goal)!=0)
+			return dirs[i];
+		map[jps[i]]='C';
+	}
+	return 0;
+}
+int dir_offset(enum dir d,int w)
+{
+	d=9-d;
+	return (1-d%3)+(d/3-1)*w;
 }
 int main(int argc,char **argv)
 {
@@ -45,18 +104,16 @@ int main(int argc,char **argv)
 	srand(time(NULL));
 
 	randomize_map(map,WIDTH,HEIGHT);
-	int s=rand()%AREA;
-	map[s]='5';
-	printf("Start: %d\n",s);
-	for (int i=2;i<=8;i+=2) {
-		int n=jump[i](map,WIDTH,HEIGHT,s);
-		if (n>=0)
-			map[n]='0'+i;
-	}
-	for (int i=1;i<=9;i+=2) {
-		int n=jump[i](map,WIDTH,HEIGHT,s);
-		if (n>=0)
-			map[n]='0'+i;
+	int s=rand()%AREA,g=rand()%AREA;
+	map[s]='O';
+	map[g]='X';
+	for (;;) {
+		enum dir d=path(map,WIDTH,HEIGHT,s,g);
+		s+=dir_offset(d,WIDTH);
+		if (s!=g)
+			map[s]='.';
+		else
+			break;
 	}
 	print_map(map,WIDTH,HEIGHT);
 	return 0;
