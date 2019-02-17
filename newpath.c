03148106165b023d0bafb7d64a415497f2c814ee
Armok628 <Mark.Johnson.62899@tamu.edu>
2019-02-16 19:22:17 -0600

    Fixes

--- newpath.c
+++ newpath.c
@@ -21,7 +21,7 @@ void randomize_map(char *m,int w,int h)
 		m[i]='#';
 	for (int x=1;x<w-1;x++)
 	for (int y=1;y<h-1;y++)
-		if (rand()%20)
+		if (rand()%4)
 			m[x+y*w]=' ';
 }
 void print_map(char *m,int w,int h)
@@ -30,23 +30,28 @@ void print_map(char *m,int w,int h)
 	for (int i=0;i<a;i++) {
 		switch (m[i]) {
 		case '#':
-			printf("\033[1;30m#");
+			printf("\033[1;30m");
 			break;
 		case '.':
-			printf("\033[1;33m.");
+			printf("\033[1;33m");
 			break;
 		case 'O':
-			printf("\033[1;34mO");
+			printf("\033[1;34m");
 			break;
 		case 'X':
-			printf("\033[1;31mX");
+			printf("\033[1;31m");
+			break;
+		case '?':
+			printf("\033[1;32m");
 			break;
 		default:
-			printf("\033[m%c",m[i]);
+			printf("\033[m");
 		}
+		putchar(m[i]);
 		if (i%w==w-1)
 			putchar('\n');
 	}
+	printf("\033[m");
 }
 void swap(int *a,int *b)
 {
@@ -55,14 +60,20 @@ void swap(int *a,int *b)
 	*b=c;
 }
 enum dir path(char *map,int w,int h,int start,int goal)
-{
+{ // TODO: Tidy up, move to jump.c
 	int dirs[10],jps[10],dists[10];
 	for (int i=0;i<10;i++) {
 		dirs[i]=i;
 		jps[i]=jump[i](map,w,h,start,goal);
 		if (jps[i]>=0) {
-			if (jps[i]==goal)
+			if (jps[i]==goal) {
+				// printf("Found goal 1 jump from %d\n",start);
+				// Clean up
+				for (int i=0;i<w*h;i++)
+					if (map[i]=='?')
+						map[i]=' ';
 				return i;
+			}
 			dists[i]=dist(jps[i],goal,w);
 		} else
 			dists[i]=-1;
@@ -74,20 +85,21 @@ enum dir path(char *map,int w,int h,int start,int goal)
 			swap(&dirs[j-1],&dirs[j]);
 			swap(&jps[j-1],&jps[j]);
 		}
-	// Print nodes
+	/* // Print nodes
 	for (int i=0;i<9;i++) {
 		if (jps[i]>-1)
 			printf("Jump point from %d: %d (%d)\n",start,jps[i],dists[i]);
 	}
+	*/
 	// Check each jump point for viable path
 	for (int i=0;i<9;i++) {
 		if (jps[i]<0)
 			continue;
 		if (dists[i]==0)
 			return dirs[i];
+		map[jps[i]]='?';
 		if (path(map,w,h,jps[i],goal)!=0)
 			return dirs[i];
-		map[jps[i]]='C';
 	}
 	return 0;
 }
@@ -107,14 +119,17 @@ int main(int argc,char **argv)
 	int s=rand()%AREA,g=rand()%AREA;
 	map[s]='O';
 	map[g]='X';
-	for (;;) {
-		enum dir d=path(map,WIDTH,HEIGHT,s,g);
+	clock_t t=clock();
+	enum dir d=path(map,WIDTH,HEIGHT,s,g);
+	t=clock()-t;
+	while (s!=g) {
+		d=path(map,WIDTH,HEIGHT,s,g);
 		s+=dir_offset(d,WIDTH);
+		//printf("New step: %d\n",s);
 		if (s!=g)
 			map[s]='.';
-		else
-			break;
 	}
 	print_map(map,WIDTH,HEIGHT);
+	printf("First iteration of path() took %fms\n",1000.0*t/CLOCKS_PER_SEC);
 	return 0;
 }
