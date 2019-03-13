a9f09af588764a4c514d675c4b1f25d65c241ae6
Armok628 <Mark.Johnson.62899@tamu.edu>
2019-03-13 03:25:14 -0500

    Error check

--- passchk.tcl
+++ passchk.tcl
@@ -2,4 +2,6 @@
 if {!$argc} {exit 1}
 set sum [string toupper [string range [exec echo -n [lindex $argv 0] | sha1sum] 0 39]]
 set hlist [dict create {*}[split [exec curl -s -S https://api.pwnedpasswords.com/range/[string range $sum 0 4]] "\n:"]]
-puts [dict get $hlist [string range $sum 5 39]]
+if {[catch {puts [dict get $hlist [string range $sum 5 39]]}]} {
+	puts 0
+}
