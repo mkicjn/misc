1b0197b2177131f543082e69e3d503482b496133
Mark Johnson <Mark.Johnson.62899@tamu.edu>
2019-03-12 23:22:34 -0500

    Old pathfinding algorithm optimization

--- oldpath.c
+++ oldpath.c
@@ -25,13 +25,17 @@ int *plan_path(char *map,const int w,const int h,int start,int goal)
 	m[start]=-1;
 	m[goal]=0;
 	bool possible=true;
+	int x_b[2]={goal%w,goal%w};
+	int y_b[2]={goal/w,goal/w};
 	for (int d=1;possible&&m[start]<0;d++) {
 		if (VISUALIZE) {
 			print_distmap(m,w,h);
 			clkslp(200);
 		}
 		possible=false;
-		for (int i=0;i<w*h;i++) {
+		for (int x=x_b[0];x<=x_b[1];x++)
+		for (int y=y_b[0];y<=y_b[1];y++) {
+			int i=x+y*w;
 			if (m[i]!=d-1)
 				continue;
 			for (int dx=-1;dx<=1;dx++)
@@ -46,6 +50,14 @@ int *plan_path(char *map,const int w,const int h,int start,int goal)
 				}
 			}
 		}
+		if (x_b[0]>0)
+			x_b[0]--;
+		if (x_b[1]<w-1)
+			x_b[1]++;
+		if (y_b[0]>0)
+			y_b[0]--;
+		if (y_b[1]<h-1)
+			y_b[1]++;
 	}
 	return m;
 }
