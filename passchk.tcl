02baee978872c621794b3cdc04f755c235e691c4
Armok628 <Mark.Johnson.62899@tamu.edu>
2019-03-13 03:24:04 -0500

    Password checker

---
+++ passchk.tcl
@@ -0,0 +1,5 @@
+#!/usr/bin/tclsh
+if {!$argc} {exit 1}
+set sum [string toupper [string range [exec echo -n [lindex $argv 0] | sha1sum] 0 39]]
+set hlist [dict create {*}[split [exec curl -s -S https://api.pwnedpasswords.com/range/[string range $sum 0 4]] "\n:"]]
+puts [dict get $hlist [string range $sum 5 39]]
