df8da89a2e97155294c98ae82ff028625b4c7046
Armok628 <Mark.Johnson.62899@tamu.edu>
2019-10-28 21:13:11 -0500

    Add CPP abuse test

---
+++ prototypes/cppabuse.h
@@ -0,0 +1,20 @@
+#define EVAL1(...) __VA_ARGS__
+
+#define TESTS(INI,X,FIN) \
+	INI() \
+	X(a,1) \
+	X(b,2) \
+	X(c,3) \
+	FIN()
+
+#define BL
+#define POSTPONE(x) x BL
+#define M_INI() POSTPONE(D) (NULL,
+#define M(n,v) n,v) POSTPONE(D) (&n,
+#define M_FIN() tail,NULL)
+
+#define D(p,n,v) n = {.prev = p, .val = v};
+
+TESTS(M_INI,M,M_FIN)
+
+EVAL1(TESTS(M_INI,M,M_FIN))
