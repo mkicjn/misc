8d658a74c9a1ff300f0971ef3bcb25dd97a9f9f6
Armok628 <Mark.Johnson.62899@gmail.com>
2019-12-20 19:49:23 -0600

    Begin rewrite

--- sfth.tcl
+++ sfth.tcl
@@ -1,193 +1,59 @@
 #!/usr/bin/tclsh
-global stack; set stack {}
-proc push {args} {
-	lappend ::stack {*}$args
+
+proc lpush {stackvar args} {
+	upvar 1 $stackvar stack
+	lappend $stackvar {*}$args
+	return
 }
-proc pop {{n 1}} {
-	global stack
+proc lpop {stackvar {n 1}} {
+	upvar 1 $stackvar stack
 	incr n -1
-	set values [lrange $stack end-$n end]
+	set vals  [lrange   $stack end-$n end]
 	set stack [lreplace $stack end-$n end]
-	return $values
+	return $vals
 }
 
-proc bind {args} {
-	set vals [pop [llength $args]]
-	uplevel 1 [list lassign $vals {*}$args]
+global stack
+set    stack [list]
+
+global forth-wordlist
+set    forth-wordlist [dict create]
+
+global context
+set    context [list forth-wordlist]
+
+proc prim {name body} {
+	global   forth-wordlist
+	dict set forth-wordlist $name [list {} $body]
+}
+proc bind {name ops args} {
+	prim $name [subst {
+		global stack
+		lpush stack {*}\[$args {*}\[lpop stack $ops]]
+	}]
 }
 
-proc define {name} {
-	set ::latest $name
-	set ::imm($name) 0
-}
+bind + 2 tcl::mathop::+
+bind . 1 puts -nonewline
 
-proc prim {name body {imm ""}} {
-	# Create a primitive with a given name and body
-	define $name
-	set ::prim($name) "{} {$body}"
-	set ::imm($name) [expr {$imm ne ""}]
-}
-
-proc op2 {op {pre {}}} {
-	# Create a primitive body representing a binary operator
-	return "bind a b; push \[expr {${pre}(\$a $op \$b)}]"
-}
-
-prim + [op2 +]
-prim - [op2 -]
-prim * [op2 *]
-prim / [op2 /]
-prim % [op2 %]
-prim LSHIFT [op2 <<]
-prim RSHIFT [op2 >>]
-prim < [op2 < -]
-prim > [op2 > -]
-prim = [op2 = -]
-
-prim /MOD {bind a b; push [expr {$a%$b}] [expr {$a/$b}]}
-
-proc op1 {pre {post {}}} {
-	# Create a primitive body representing a unary operator
-	return "set a \[pop]; push \[expr {${pre}(\$a $post)}]"
-}
-
-prim INVERT [op1 ~]
-prim NEGATE [op1 -]
-
-prim DUP {bind a; push $a $a}
-prim DROP pop
-prim SWAP {bind a b; push $b $a}
-prim ROT {bind a b c; push $b $c $a}
-
-prim . {puts -nonewline "[pop] "}
-prim CR {puts ""}
-
-global line; set line [list]
-proc word {} {
-	global line
-	set line [lassign $line word]
-	return $word
-}
-prim REFILL {set ::line [gets stdin]; push [expr {-[eof stdin]}]}
-prim PARSE-NAME {push [word]}
-
-global latest; set latest {}
-global imm; array set imm [list]
-prim START-DEFINITION {define [pop]}
-prim IMMEDIATE {set ::imm($::latest) 1}
-
-prim ! {
-	bind val name
-	set i [lassign $name name]
-	lset ::colon($name) {*}$i $val
-}
-prim @ {
-	bind name
-	set i [lassign $name name]
-	push [lindex $::colon($name) {*}$i]
-}
-proc compile {args} {lappend ::colon($::latest) {*}$args}
-prim , {compile [pop]}
-
-prim BRANCH {
-	uplevel 1 {
-		incr i [lindex $colon($name) $i]
-	}
-}
-prim 0BRANCH {
-	if {[pop] == 0} {
-		tailcall apply $::prim(BRANCH)
-	} else {
-		uplevel 1 {incr i}
-	}
-}
-prim EXIT {return -code break}
-prim LIT {
-	uplevel 1 {
-		push [lindex $colon($name) $i]
-		incr i
-	}
-}
-prim COMPARE {bind a b; push [string compare $a $b]}
-prim BYE exit
-
-prim GO-TO {uplevel 1 {lassign [lindex $colon($name) $i] name i}}
-prim <BUILDS {
-	define [word]
-	compile LIT [list $::latest 4]
-	compile EXIT ""
-}
-prim DOES> {
-	uplevel 1 {
-		lset ::colon($::latest) 2 GO-TO
-		lset ::colon($::latest) 3 [list $name $i]
-		return -code break
-	}
-}
-prim SEE {puts $::colon([word])}
-prim .S {puts "<[llength $::stack]> $::stack"}
-
-proc execute {name} {
-	global prim colon
-	if {[info exists prim($name)]} {
-		uplevel 1 [list apply $prim($name)]
-	} elseif {[info exists colon($name)]} {
-		set i 0
-		while {[set word [lindex $colon($name) $i]] ne ""} {
-			incr i
-			set errcode [catch {execute $word} errmsg]
-			if {$errcode == 0} continue
-			if {[string first break $errmsg] >= 0} break
-			error "$name->$word: $errmsg"
+proc interpret {line} {
+	global stack
+	global context
+	foreach word $line {
+		set def ""
+		foreach wlistvar $context {
+			upvar 1 $wlistvar wlist
+			if {![catch {set def [dict get $wlist $word]}]} break
 		}
-	} else {
-		error "$name?"
-	}
-}
-
-global state; set state 0
-prim \] {set ::state 1}
-prim \[ {set ::state 0} immediate
-prim : {define [word]; set ::state 1}
-prim \; {compile EXIT; set ::state 0} immediate
-prim INTERPRET-LINE {
-	global state imm
-	while {[llength $::line]} {
-		set name [word]
-		if {[string is integer $name]} {
-			if {$state} {
-				compile LIT $name
-			} else {
-				push $name
-			}
-		} elseif {[info exists imm($name)]} {
-			if {$state && !$imm($name)} {
-				compile $name
-			} else {
-				execute $name
-			}
+		if {$def ne ""} {
+			apply $def
+		} elseif {[string is entier $word]} {
+			lpush stack $word
 		} else {
-			error "$name?"
+			return -code error "$word?"
 		}
 	}
-}
-prim QUIT {
-	global prim
-	while {1} {
-		apply $prim(REFILL)
-		if {[pop]} break
-		if {[catch {apply $prim(INTERPRET-LINE)} err]} {
-			puts $err
-		} elseif {$::state} {
-			puts "compiled"
-		} else {
-			puts "ok"
-		}
-	}
+	puts " ok"
 }
 
-if {$tcl_interactive} return
-apply $prim(QUIT)
-#foreach name [array names colon] {
-#	puts ": $name $colon($name) ; [expr {$imm($name)?"IMMEDIATE":""}]"
-#}
+interpret [gets stdin]
