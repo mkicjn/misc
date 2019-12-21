702ff6993d343b2c2b01e428675ac22d3d7cf660
Armok628 <Mark.Johnson.62899@gmail.com>
2019-12-20 22:14:21 -0600

    Add SGR and friends

--- aterm.h
+++ aterm.h
@@ -2,22 +2,79 @@
 #define ATERM_H
 
 #define CSI "\033["
-#define CUP "H"
-#define ED  "J"
-#define SGR "m"
-#define SCP "s"
-#define RCP "u"
 
 #define CUU "A"
 #define CUD "B"
 #define CUF "C"
 #define CUB "D"
+#define CNL "E"
+#define CPL "F"
+#define CHA "G"
+#define CUP "H"
+
+#define ED  "J"
+#define EL  "K"
+#define SU  "S"
+#define SD  "T"
 
 #define CUS "?25h" 
 #define CUH "?25l"
+#define DSR "6n"
+#define SCP "s"
+#define RCP "u"
 
-#define CLS "2" ED
+#define CLB  "1" ED
+#define CLS  "2" ED
+#define CLSB "3" ED
 
-#define AT_XY(X,Y) CSI #X ";" #Y CUP
+#define EALTB "1049h"
+#define DALTB "1049l"
+
+#define SEP ";"
+#define AT_XY(X,Y) CSI #X SEP #Y CUP
+
+#define SGR "m"
+
+#define RESET         "0"
+#define BOLD          "1"
+#define FAINT         "2"
+#define ITALIC        "3"
+#define UNDERLINE     "4"
+#define SLOWBLINK     "5"
+#define FASTBLINK     "6"
+#define REVERSE       "7"
+#define CONCEAL       "8"
+#define STRIKETHRU    "9"
+#define DEF_FONT     "10"
+#define FONT(N)      "1" #N
+#define FRAKTUR      "20"
+#define DUB_UNDERLN  "21"
+#define NORMAL_COLR  "22"
+#define NOITALIC     "23"
+#define NOUNDERLN    "24"
+#define NOBLINK      "25"
+#define NOINVERSE    "27"
+#define REVEAL       "28"
+#define NOSTRKTHRU   "29"
+
+#define BLACK    "0"
+#define RED      "1"
+#define GREEN    "2"
+#define YELLOW   "3"
+#define BLUE     "4"
+#define MAGENTA  "5"
+#define CYAN     "6"
+#define GRAY     "7"
+
+#define FG_COLR(S)   "3" S
+#define DEF_FG_COLR  "39"
+#define BG_COLR(S)   "4" S
+#define DEF_BG_COLR  "49"
+
+#define FG_BCOLR(S)  "9" S
+#define BG_BCOLR(S) "10" S
+
+#define RGB(R,G,B)   "8;2;" #R SEP #G SEP #B
+#define COLR_8BIT(N) "8;5;" #N
 
 #endif
