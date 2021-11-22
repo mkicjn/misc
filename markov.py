bbcf2fa3fb1cfca4aa4b90797c372c0a97f40f42
Mark Johnson <mijohnson@aggienetwork.com>
2021-11-21 20:45:48 -0600

    Revise markov.py to operate on characters

--- markov.py
+++ markov.py
@@ -3,44 +3,36 @@ import sys
 import re
 import random
 
-# This script is optimized for English text, but it can be tweaked to work on other things.
+(_, output_size, memory) = sys.argv
 
-gen_count = 128
-if len(sys.argv) > 1:
-    gen_count = int(sys.argv[1])
+output_size = int(output_size)
+memory = int(memory)
 
-memory = 1
-if len(sys.argv) > 2:
-    memory = int(sys.argv[2])
-
-# Build a list of input words
-input_words = re.findall(r'[\w]+\s?|[^\w]\s?', re.sub(r'\s+', ' ', sys.stdin.read()))
+# Build a list of input syms
+input_syms = sys.stdin.read()
 
 # Construct the vocabulary and weights
-last_words = (None,) * memory
+history = (None,) * memory
 vocab = {}
 
-for word in input_words:
-    if last_words not in vocab:
-        vocab[last_words] = {}
-    if word not in vocab[last_words]:
-        vocab[last_words][word] = 0
-    vocab[last_words][word] += 1
-    last_words = last_words[1:] + (word,)
-# TODO: Try tracking for multiple previous words
+for symbol in input_syms:
+    if history not in vocab:
+        vocab[history] = {}
+    if symbol not in vocab[history]:
+        vocab[history][symbol] = 0
+    vocab[history][symbol] += 1
+    history = history[1:] + (symbol,)
 
-# Generate random strings according to the vocabulary's weights
-last_words = random.choice(list(vocab.keys()))
-for i in range(0, gen_count):
+# Generate a random string according to the vocabulary's weights
+history = random.choice(list(vocab.keys()))
+for i in range(output_size):
     try:
-        word = random.choices(list(vocab[last_words].keys()),
-                      weights=list(vocab[last_words].values()), k=1)[0]
+        symbol = random.choices(list(vocab[history].keys()),
+                        weights=list(vocab[history].values()))[0]
     except:
         print('\n')
-        last_words = random.choice(list(vocab.keys()))
+        history = random.choice(list(vocab.keys()))
         continue
-    #if re.match('\w+', word) is not None:
-    #    print(' ', end='')
-    print(word, end='')
-    last_words = last_words[1:] + (word,)
+    print(symbol, end='')
+    history = history[1:] + (symbol,)
 print()
