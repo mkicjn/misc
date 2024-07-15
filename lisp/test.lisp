26da4a84b341698f74ba326415d8696f3ec6cb18
mkicjn <mkicjn@gmail.com>
2024-07-14 21:38:57 -0500

    Add demonstration of GC-vs-eq? issue

--- lisp/test.lisp
+++ lisp/test.lisp
@@ -7,3 +7,10 @@
 
 ; Testing sequential let bindings
 (let ((a '1) (b (cons a '2)) (a '0)) (cons a b))
+
+; Demo: Issue with GC - deep copying prevents consistent equality checking for cells
+(let ((a '(1))          (b a) (c '(2))) (eq? a b))
+(let ((a (cons '1 '())) (b a) (c '(2))) (eq? a b))
+; Same issue without let
+((lambda (x) ((lambda (a b) (eq? a b)) x x)) (cons '1 '()))
+((lambda (p) (eq? (car p) (cdr p))) ((lambda (x) (cons x x)) (cons '1 '())))
