f89c1be893c38503aa70c2bc6ebd2a8a87a16b13
mkicjn <mkicjn@gmail.com>
2024-11-05 18:07:15 -0600

    Rewrite bytebeat.c interpreter

--- bytebeat.c
+++ bytebeat.c
@@ -1,42 +1,81 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 // Try this:
 // ./a.out 't5*t7>&t3*t10>&|' | pacat --rate=8000
 
-char interpret(char *i, int t)
+char interpreter(char *s, int t)
 {
-	long stack[100], *s = stack, n = 0, v = 0;
+	// This is dangerously close to a FALSE interpreter...
+	intptr_t space[100], *stack = &space[100], n = 0;
+	bool n_valid = false;
 	for (;;) {
-		char c = *(i++);
-		if (c >= '0' && c <= '9') {
-			n = n * 10 + c - '0', v = 1;
+		// Parse numbers character-by-character
+		char c = *(s++);
+		if ('0' <= c && c <= '9') {
+			n = (n * 10) + (c - '0');
+			n_valid = true;
 			continue;
-		} else if (v) {
-			*(s++) = n;
-			n = 0, v = 0;
+		} else if (n_valid) {
+			*(--stack) = n;
+			n_valid = false;
+			n = 0;
 		}
-		if (!c)
-			break;
+		// Interpret operations
 		switch (c) {
-		case 't': *(s++) = t; break;
-		case '$': s[0] = s[-1]; s++; break;
+		case '\0':
+			return stack[0];
+		case 't':
+			--stack;
+			stack[0] = t;
+			break;
+		case '$':
+			--stack;
+			stack[0] = stack[1];
+			break;
+		case '_':
+			stack[0] = -stack[0];
+			break;
+		case '"':
+			--stack;
+			stack[0] = (intptr_t)s;
+			while (*(s++) != '"')
+				if (*s == '\0')
+					break;
+			break;
+		case '@':
+			stack[1] = ((char *)stack[1])[stack[0]];
+			stack++;
+			break;
+#define BIN_OP(C, X) \
+		case C: \
+			stack[1] = (stack[1] X stack[0]); \
+			stack++; \
+			break;
 
-		case '<': s--; s[-1] <<= s[0]; break;
-		case '>': s--; s[-1] >>= s[0]; break;
-		case '+': s--; s[-1]  += s[0]; break;
-		case '-': s--; s[-1]  -= s[0]; break;
-		case '*': s--; s[-1]  *= s[0]; break;
-		case '/': s--; s[-1]  /= s[0]; break;
-		case '%': s--; s[-1]  %= s[0]; break;
-		case '&': s--; s[-1]  &= s[0]; break;
-		case '|': s--; s[-1]  |= s[0]; break;
-		case '^': s--; s[-1]  ^= s[0]; break;
-
-		case '"': *(s++) = (long)i; while (*(i++) != '"'); break;
-		case ']': s--; s[-1] = ((char *)s[-1])[s[0]];
+			BIN_OP('+', +)
+			BIN_OP('-', -)
+			BIN_OP('*', *)
+			BIN_OP('%', %)
+			BIN_OP('^', ^)
+			BIN_OP('&', &)
+			BIN_OP('|', |)
+			BIN_OP('<', <<)
+			BIN_OP('>', >>)
+			BIN_OP('=', ==)
+			BIN_OP('!', !=)
+#undef BIN_OP
 		}
+
+		/*
+		// Debug stack printing
+		for (int i = 0; i < (&space[100] - stack); i++) {
+			fprintf(stderr, "%ld ", stack[i]);
+		}
+		fprintf(stderr, "\n");
+		*/
 	}
-	return s[-1];
 }
 
 int main(int argc, char **argv)
@@ -45,5 +84,5 @@ int main(int argc, char **argv)
 	if (argc < 2)
 		return 1;
 	for (;;)
-		putchar(interpret(argv[1], t++));
+		putchar(interpreter(argv[1], t++));
 }
