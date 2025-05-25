45c93de4d23c14709a6c65f0d825c85ed641f5e2
mkicjn <mkicjn@gmail.com>
2025-05-25 17:03:57 -0500

    Use numpy arrays

--- string_dp.py
+++ string_dp.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
 import sys
+import numpy as np
 
 def levenshtein(s1, s2):
     s1 = ' ' + s1
     s2 = ' ' + s2
-    f = [[[0] for j in range(len(s2))] for i in range(len(s1))]
-
-    f[0][0] = 0
-    for i in range(1, len(s1)):
-        f[i][0] = i
-    for j in range(1, len(s2)):
-        f[0][j] = j
+    f = np.zeros((len(s1), len(s2)))
 
     for i in range(1, len(s1)):
         for j in range(1, len(s2)):
-            f[i][j] = min(
-                    f[i][j-1] + 1,
-                    f[i-1][j] + 1,
-                    f[i-1][j-1] + (1 if s1[i] != s2[j] else 0))
+            f[i, j] = min(
+                    f[i, j-1] + 1,
+                    f[i-1, j] + 1,
+                    f[i-1, j-1] + (1 if s1[i] != s2[j] else 0))
     return f
 
 def show_mat(f, s1, s2):
@@ -30,45 +25,33 @@ def show_mat(f, s1, s2):
     for i in range(len(f)):
         print(s1[i], end='')
         for j in range(len(f[i])):
-            print('{:3d}'.format(f[i][j]), end='')
+            print('{:3d}'.format(f[i, j]), end='')
         print('')
 
 def lcs(s1, s2):
     s1 = ' ' + s1
     s2 = ' ' + s2
-    f = [[[0] for j in range(len(s2))] for i in range(len(s1))]
-
-    f[0][0] = 0
-    for i in range(1, len(s1)):
-        f[i][0] = 0
-    for j in range(1, len(s2)):
-        f[0][j] = 0
+    f = np.zeros((len(s1), len(s2)))
 
     for i in range(1, len(s1)):
         for j in range(1, len(s2)):
             if s1[i] == s2[j]:
-                f[i][j] = f[i-1][j-1] + 1
+                f[i, j] = f[i-1, j-1] + 1
             else:
-                f[i][j] = max(f[i][j-1], f[i-1][j])
+                f[i, j] = max(f[i, j-1], f[i-1, j])
     return f
 
 def custom_lcs(s1, s2):
     s1 = ' ' + s1
     s2 = ' ' + s2
-    f = [[[0] for j in range(len(s2))] for i in range(len(s1))]
-
-    f[0][0] = 0
-    for i in range(1, len(s1)):
-        f[i][0] = 0
-    for j in range(1, len(s2)):
-        f[0][j] = 0
+    f = np.zeros((len(s1), len(s2)))
 
     for i in range(1, len(s1)):
         for j in range(1, len(s2)):
             if s1[i] == s2[j] and i != j:
-                f[i][j] = f[i-1][j-1] + 1
+                f[i, j] = f[i-1, j-1] + 1
             else:
-                f[i][j] = max(f[i-1][j-1], f[i][j-1], f[i-1][j], 0) // 2
+                f[i, j] = max(f[i-1, j-1], f[i, j-1], f[i-1, j], 0) // 2
     return f
 
 def reassociate(s1, f):
