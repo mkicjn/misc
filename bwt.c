df58742e34e3230bbcfb70b3e037c6e92d710cca
mkicjn <mkicjn@gmail.com>
2025-05-31 20:53:56 -0500

    Slight refactor of bwt.c

--- bwt.c
+++ bwt.c
@@ -5,8 +5,6 @@
 #include <string.h>
 
 size_t row_len;
-size_t n_rows;
-char **rows;
 int rowcmp(const void *row1_ptr, const void *row2_ptr)
 {
 	const char *row1 = *(const char **)row1_ptr;
@@ -14,33 +12,42 @@ int rowcmp(const void *row1_ptr, const void *row2_ptr)
 	return memcmp(row1, row2, row_len);
 }
 
+char **rows;
+char *rowbuf;
+void bwt(const char *data, size_t data_len)
+{
+	// Allocates rowbuf as char[data_len * 2], rows as char (*[data_len])[data_len]
+	size_t rowbuf_len = data_len * 2;
+	char *rowbuf = NULL;
+
+	rowbuf = malloc(rowbuf_len + 1);
+	memcpy(&rowbuf[0],        data, data_len);
+	memcpy(&rowbuf[data_len], data, data_len);
+	rowbuf[rowbuf_len] = '\0';
+
+	size_t n_rows = data_len;
+	rows = malloc(n_rows * sizeof(*rows));
+	for (int i = 0; i < n_rows; i++)
+		rows[i] = &rowbuf[i];
+
+	row_len = data_len;
+	qsort(rows, n_rows, sizeof(*rows), rowcmp);
+}
+
 int main(int argc, char **argv)
 {
 	if (argc < 2)
 		return 1;
 
-	size_t arg_len = strlen(argv[1]);
-	size_t data_len = arg_len * 2;
-	char *data = NULL;
+	const char *data = argv[1];
+	size_t data_len = strlen(argv[1]);
+	bwt(data, data_len);
 
-	data = malloc(data_len + 1);
-	memcpy(&data[0],       argv[1], arg_len);
-	memcpy(&data[arg_len], argv[1], arg_len);
-	data[data_len] = '\0';
-
-	row_len = arg_len;
-	n_rows = arg_len;
-	rows = malloc(n_rows * sizeof(*rows));
-	for (int i = 0; i < n_rows; i++)
-		rows[i] = &data[i];
-
-	qsort(rows, n_rows, sizeof(*rows), rowcmp);
-
-	for (int i = 0; i < n_rows; i++)
-		putchar(rows[i][arg_len - 1]);
+	for (int i = 0; i < data_len; i++)
+		putchar(rows[i][data_len - 1]);
 	putchar('\n');
 
 	free(rows);
-	free(data);
+	free(rowbuf);
 	return 0;
 }
