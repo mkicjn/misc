e720f4ebca8200947bbd59f5eba07ffd035061f5
mkicjn <mkicjn@gmail.com>
2025-06-03 23:15:59 -0500

    Add generic splay tree experiment

---
+++ splay_generic.c
@@ -0,0 +1,289 @@
+//$(which tcc) $CFLAGS -run $0 $@; exit $?
+
+// Generic splay tree API experiment
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct splay_link {
+	struct splay_link *child[2];
+};
+
+enum splay_dir {
+	// (used for child indexing)
+	LEFT  = 0,
+	RIGHT = 1,
+	// (not used for child indexing)
+	HERE,
+	NOWHERE,
+};
+
+typedef enum splay_dir (*splay_nav_fn)(const struct splay_link *n, const void *nav_arg);
+
+/*
+ * Move x where p was and swap links around
+ * e.g., rotate(&g->child[LEFT], RIGHT);
+ *
+ *      (g)           (g)
+ *      / \           / \
+ *     p   d   ->    x   d
+ *    / \           / \
+ *   a   x         p   c
+ *      / \       / \
+ *     b   c     a   b
+ *
+ * All types of splays have this in common
+ */
+void rotate(struct splay_link **p_ptr, int x_dir)
+{
+	struct splay_link *p = *p_ptr;
+	struct splay_link *x = p->child[x_dir];
+	p->child[x_dir] = x->child[1-x_dir];
+	x->child[1-x_dir] = p;
+	*p_ptr = x;
+}
+
+// Top-down strategy
+bool splay(struct splay_link **root_ptr, splay_nav_fn nav, const void *nav_arg)
+{
+	if (*root_ptr == NULL)
+		return false;
+
+	struct splay_link *subtree[2] = {NULL, NULL};
+	struct splay_link **leaf[2] = {&subtree[0], &subtree[1]};
+
+	for (;;) {
+		struct splay_link *g = *root_ptr;
+		enum splay_dir dir = nav(g, nav_arg);
+		if (dir == HERE)
+			break;
+
+		struct splay_link *p = g->child[dir];
+		if (nav(p, nav_arg) == dir) {
+			g = p;
+			rotate(root_ptr, dir);
+		}
+
+		if (g->child[dir] == NULL)
+			break;
+		*leaf[1-dir] = g;
+		leaf[1-dir] = &g->child[dir];
+		*root_ptr = g->child[dir];
+	}
+
+	struct splay_link *g = *root_ptr;
+	*leaf[LEFT] = g->child[LEFT];
+	*leaf[RIGHT] = g->child[RIGHT];
+	g->child[LEFT] = subtree[LEFT];
+	g->child[RIGHT] = subtree[RIGHT];
+	return nav(g, nav_arg) == HERE;
+}
+
+bool find(struct splay_link **root_ptr, splay_nav_fn nav, const void *nav_arg)
+{
+	if (!splay(root_ptr, nav, nav_arg)) {
+		printf("lookup failed\n");
+		return false;
+	} else {
+		return true;
+	}
+}
+
+bool insert(struct splay_link **root_ptr, splay_nav_fn nav, struct splay_link *x)
+{
+	if (*root_ptr == NULL) {
+		*root_ptr = x;
+		return true;
+	}
+
+	if (splay(root_ptr, nav, x))
+		return false;
+
+	struct splay_link *g = *root_ptr;
+	int dir = nav(g, x);
+	x->child[1-dir] = g;
+	x->child[dir] = g->child[dir];
+	g->child[dir] = NULL;
+	*root_ptr = x;
+	return true;
+}
+
+enum splay_dir splay_nav_to_min(const struct splay_link *n, const void *arg)
+{
+	(void)arg;
+	if (n == NULL)
+		return NOWHERE;
+	else if (n->child[LEFT] == NULL)
+		return HERE;
+	else
+		return LEFT;
+}
+
+bool delete(struct splay_link **root_ptr, splay_nav_fn nav, const void *nav_arg)
+{
+	if (!splay(root_ptr, nav, nav_arg))
+		return false;
+	struct splay_link *del = *root_ptr;
+
+	if (!del->child[RIGHT]) {
+		*root_ptr = del->child[LEFT];
+		return true;
+	} else if (!del->child[LEFT]) {
+		*root_ptr = del->child[RIGHT];
+		return true;
+	}
+	*root_ptr = del->child[RIGHT];
+	splay(root_ptr, splay_nav_to_min, NULL);
+	(*root_ptr)->child[LEFT] = del->child[LEFT];
+	return true;
+}
+
+
+// ******************** No longer generic ********************
+
+struct node {
+	unsigned long key;
+	struct splay_link link;
+};
+
+// (Reimplemented here for fun)
+#define OFFSET_OF(T, MEMB) ((char *)&((T *)NULL)->MEMB - (char *)((T *)NULL))
+#define CONTAINER_OF(PTR, T, MEMB) (T *)((char *)(PTR) - OFFSET_OF(T, MEMB))
+
+enum splay_dir node_nav(const struct splay_link *l1, const void *l2)
+{
+	const struct node *n1 = CONTAINER_OF(l1, struct node, link);
+	const struct node *n2 = CONTAINER_OF(l2, struct node, link);
+	if (n1 == NULL)
+		return NOWHERE;
+	else if (n2->key > n1->key)
+		return RIGHT;
+	else if (n2->key < n1->key)
+		return LEFT;
+	else
+		return HERE;
+}
+
+bool node_splay(struct splay_link **root, unsigned long key)
+{
+	const struct node dummy = (struct node){.key = key, .link = {0}};
+	return splay(root, node_nav, &dummy.link);
+}
+
+void print_node(struct splay_link *l, int depth, const char *s)
+{
+	if (l == NULL)
+		return;
+
+	struct node *n = CONTAINER_OF(l, struct node, link);
+	for (int i = 1; i < depth; i++)
+		printf("| ");
+	printf("%s%lu\n", s, n->key);
+
+	print_node(l->child[LEFT], depth+1, "L:");
+	print_node(l->child[RIGHT], depth+1, "R:");
+}
+
+void show_tree(struct splay_link *l)
+{
+	print_node(l, 0, "");
+	printf("----------------------------------------\n");
+}
+
+int main(int argc, char **argv)
+{
+	// Base case testing
+#define NODE(X, L, R) &(struct node){.key=(X), .link=(struct splay_link){.child={(L), (R)}}}.link
+#define LEAF(X) NODE(X, NULL, NULL)
+	struct splay_link *root;
+
+	root = 
+		NODE(3,
+			NODE(2,
+				NODE(1,
+					NULL,
+					NULL),
+				NULL),
+			NULL);
+
+	show_tree(root);
+	node_splay(&root, 1);
+	show_tree(root);
+
+	root = 
+		NODE(3,
+			NODE(1,
+				NULL,
+				NODE(2,
+					NULL,
+					NULL)
+			),
+			NULL
+		);
+
+	show_tree(root);
+	node_splay(&root, 2);
+	show_tree(root);
+
+	// Test splaying effect on unfavorably balanced trees
+	
+	// Oops, all zig-zig
+	root =
+		NODE(10,
+			NODE(9,
+				NODE(8,
+					NODE(7,
+						NODE(6,
+							NODE(5,
+								NODE(4,
+									NODE(3,
+										NODE(2,
+											NODE(1,
+												NULL,
+												NULL),
+											NULL),
+										NULL),
+									NULL),
+								NULL),
+							NULL),
+						NULL),
+					NULL),
+				NULL),
+			NULL);
+
+	show_tree(root);
+	node_splay(&root, 1);
+	show_tree(root);
+
+	// Oops, all zig-zag
+	root =
+		NODE(10,
+			NODE(1,
+				NULL, 
+				NODE(9,
+					NODE(2,
+						NULL,
+						NODE(8,
+							NODE(3,
+								NULL,
+								NODE(7,
+									NODE(4,
+										NULL,
+										NODE(6,
+											NODE(5,
+												NULL,
+												NULL),
+											NULL)),
+									NULL)),
+							NULL)),
+					NULL)),
+			NULL);
+
+	show_tree(root);
+	node_splay(&root, 5);
+	show_tree(root);
+
+	return 0;
+}
