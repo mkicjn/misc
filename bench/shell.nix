a77473174404722c83987078c86650cc2ad6d1a0
mkicjn <mkicjn@gmail.com>
2026-01-05 18:20:53 -0600

    mkShellNoCC -> mkShell

--- bench/shell.nix
+++ bench/shell.nix
@@ -2,7 +2,7 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
-pkgs.mkShellNoCC {
+pkgs.mkShell {
   packages = with pkgs; [
     gforth
     python3
