fe6561c647b7ccae062e4bb379e9d0fb862b7d21
mkicjn <mkicjn@gmail.com>
2026-03-26 18:22:10 -0500

    Better drawing

--- rtrl/proto.c
+++ rtrl/proto.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <poll.h>
 #include <time.h>
+#include <limits.h>
 
 /* ******** Time Utilities ******** */
 
@@ -19,7 +20,7 @@ unsigned long msec(void)
 /* ******** Screen Utilities ******** */
 
 #define WIDTH 80
-#define HEIGHT 24
+#define HEIGHT 23
 
 static inline bool in_bounds(int x, int y)
 {
@@ -59,40 +60,79 @@ void screen_deinit(void)
 struct thing {
 	const char *glyph;
 	int x, y;
-	void (*think)(void *self);
+	void (*think)(struct thing *self);
 };
-
-void nop(void *self)
-{
-	(void)self;
-}
 
 #define MAX_THINGS 20
 struct thing things[MAX_THINGS];
 int num_things = 0;
 
-void put_thing(struct thing *t)
+struct thing *make_thing(const char *glyph, int x, int y, void (*think)(struct thing *self))
 {
-	if (!in_bounds(t->x, t->y))
-		return;
+	struct thing *t = &things[num_things++];
+	t->glyph = glyph;
+	t->x = x;
+	t->y = y;
+	t->think = think;
+	return t;
+}
 
-	cur_pos(t->x, t->y);
-	printf("%s", t->glyph);
+struct thing *thing_at(int x, int y)
+{
+	for (int i = 0; i < num_things; i++)
+		if (things[i].x == x && things[i].y == y)
+			return &things[i];
+	return NULL;
+}
+
+
+/* ******** Drawing ******** */
+
+void draw_glyph(const char *glyph)
+{
+	printf("%s", glyph);
 	fflush(stdout);
 }
 
-const char *ground = "\033[m_";
-void draw(void)
+void draw_thing(struct thing *t)
+{
+	if (!in_bounds(t->x, t->y))
+		return;
+	cur_pos(t->x, t->y);
+	draw_glyph(t->glyph);
+}
+
+const char *ground_glyph(int x, int y)
+{
+	(void)x;
+	(void)y;
+	return "\033[0;32m_";
+}
+
+void draw_loc(int x, int y)
+{
+	if (!in_bounds(x, y))
+		return;
+	cur_pos(x, y);
+
+	struct thing *t = thing_at(x, y);
+	if (t)
+		draw_glyph(t->glyph);
+	else
+		draw_glyph(ground_glyph(x, y));
+}
+
+void draw_all(void)
 {
 	cur_pos(0, 0);
 	for (int y = 0; y < HEIGHT; y++) {
 		for (int x = 0; x < WIDTH; x++)
-			printf("%s", ground);
+			draw_glyph(ground_glyph(x, y));
 		printf("\r\n");
 	}
 
 	for (int i = 0; i < num_things; i++)
-		put_thing(&things[i]);
+		draw_thing(&things[i]);
 }
 
 
@@ -116,7 +156,7 @@ void signal_handler(int signo)
 		break;
 	case SIGWINCH:
 		clear_screen();
-		draw();
+		draw_all();
 		break;
 	default:
 		die();
@@ -142,23 +182,11 @@ void tick()
 
 	for (int i = 0; i < num_things; i++)
 		things[i].think(&things[i]);
-
-	draw();//////////////////
 }
 
-struct thing *make_thing(const char *glyph, int x, int y, void (*think)(struct thing *self))
+bool apply_dir(unsigned char dir, int *xp, int *yp)
 {
-	struct thing *t = &things[num_things++];
-	t->glyph = glyph;
-	t->x = x;
-	t->y = y;
-	t->think = think;
-	return t;
-}
-
-void apply_dir(char dir, int *xp, int *yp)
-{
-	static const int dx[256] = {
+	static const int dx[UCHAR_MAX] = {
 		['1'] = -1, ['y'] = -1,
 		['4'] = -1, ['h'] = -1,
 		['7'] = -1, ['b'] = -1,
@@ -166,7 +194,7 @@ void apply_dir(char dir, int *xp, int *yp)
 		['6'] =  1, ['l'] =  1,
 		['9'] =  1, ['n'] =  1,
 	};
-	static const int dy[256] = {
+	static const int dy[UCHAR_MAX] = {
 		['1'] =  1, ['b'] =  1,
 		['2'] =  1, ['j'] =  1,
 		['3'] =  1, ['n'] =  1,
@@ -181,12 +209,30 @@ void apply_dir(char dir, int *xp, int *yp)
 	if (in_bounds(x, y)) {
 		*xp = x;
 		*yp = y;
+		return true;
+	} else {
+		return false;
 	}
+}
+
+void move_thing(struct thing *t, unsigned char dir)
+{
+	int x = t->x, y = t->y;
+	if (apply_dir(dir, &t->x, &t->y)) {
+		draw_loc(x, y);
+		draw_thing(t);
+	}
+}
+
+void inert(struct thing *self)
+{
+	(void)self;
 }
 
 void wander(struct thing *self)
 {
-	apply_dir('1' + rand() % 9, &self->x, &self->y);
+	move_thing(self, '1' + rand() % 9);
+	// ^ TODO: Remove rand()
 }
 
 int main()
@@ -194,18 +240,18 @@ int main()
 	install_signal_handlers();
 	screen_init();
 
-	struct thing *player = make_thing("\033[m@", WIDTH / 2, HEIGHT / 2, nop);
-	make_thing("\033[31m@",     WIDTH / 3,     HEIGHT / 3, wander);
-	make_thing("\033[31m@",     WIDTH / 3, 2 * HEIGHT / 3, wander);
-	make_thing("\033[31m@", 2 * WIDTH / 3,     HEIGHT / 3, wander);
-	make_thing("\033[31m@", 2 * WIDTH / 3, 2 * HEIGHT / 3, wander);
+	struct thing *player = make_thing("\033[0;34m@", WIDTH / 2, HEIGHT / 2, inert);
+	make_thing("\033[0;31m@",     WIDTH / 3,     HEIGHT / 3, wander);
+	make_thing("\033[0;31m@",     WIDTH / 3, 2 * HEIGHT / 3, wander);
+	make_thing("\033[0;31m@", 2 * WIDTH / 3,     HEIGHT / 3, wander);
+	make_thing("\033[0;31m@", 2 * WIDTH / 3, 2 * HEIGHT / 3, wander);
 
 
 	struct pollfd in;
 	in.fd = fileno(stdin);
 	in.events = POLLIN;
 
-	draw();
+	draw_all();
 	unsigned int reflex = 256;
 	while (!quit) {
 		int input = 0;
@@ -220,8 +266,7 @@ int main()
 			quit = true;
 			break;
 		default:
-			apply_dir(input, &player->x, &player->y);
-			put_thing(player);
+			move_thing(player, input);
 			break;
 		}
 
