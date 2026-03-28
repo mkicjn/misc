5405646748057bb39cb663d8d82388298e6900d0
mkicjn <mkicjn@gmail.com>
2026-03-27 22:53:32 -0500

    ECS refactor + better timing/movement

--- rtrl/proto.c
+++ rtrl/proto.c
@@ -54,35 +54,73 @@ void screen_deinit(void)
 	system("stty sane");
 }
 
-
-/* ******** Things ******** */
-
-struct thing {
-	const char *glyph;
-	int x, y;
-	void (*think)(struct thing *self);
-};
-
-#define MAX_THINGS 20
-struct thing things[MAX_THINGS];
-int num_things = 0;
-
-struct thing *make_thing(const char *glyph, int x, int y, void (*think)(struct thing *self))
+static inline void apply_dir(int dir, int *xp, int *yp)
 {
-	struct thing *t = &things[num_things++];
-	t->glyph = glyph;
-	t->x = x;
-	t->y = y;
-	t->think = think;
-	return t;
+	static const int dx[0x100] = {
+		['1'] = -1, ['y'] = -1,
+		['4'] = -1, ['h'] = -1,
+		['7'] = -1, ['b'] = -1,
+		['3'] =  1, ['u'] =  1,
+		['6'] =  1, ['l'] =  1,
+		['9'] =  1, ['n'] =  1,
+	};
+	static const int dy[0x100] = {
+		['1'] =  1, ['b'] =  1,
+		['2'] =  1, ['j'] =  1,
+		['3'] =  1, ['n'] =  1,
+		['7'] = -1, ['y'] = -1,
+		['8'] = -1, ['k'] = -1,
+		['9'] = -1, ['u'] = -1,
+	};
+
+	*xp += dx[dir & 0xff];
+	*yp += dy[dir & 0xff];
 }
 
-struct thing *thing_at(int x, int y)
+
+/* ******** Entities and Basic Components ******** */
+
+#define MAX_ENTITIES 1000
+long entities = 0;
+
+static inline long new_entity(void)
 {
-	for (int i = 0; i < num_things; i++)
-		if (things[i].x == x && things[i].y == y)
-			return &things[i];
-	return NULL;
+	return entities++; // TODO: OOM handling
+}
+
+// Appearance
+struct {
+	bool en;
+	const char *glyph;
+} appearance[MAX_ENTITIES];
+
+static inline void set_appearance(long e, const char *glyph)
+{
+	appearance[e].en = true;
+	appearance[e].glyph = glyph;
+}
+
+// Position
+struct {
+	bool en;
+	int x, y;
+} position[MAX_ENTITIES];
+
+static inline void set_position(long e, int x, int y)
+{
+	position[e].en = true;
+	position[e].x = x;
+	position[e].y = y;
+}
+
+long entity_at(int x, int y)
+{
+	// FIXME: Will return entities that are not visible (or any other desired property)
+	//        Need something at least marginally more sophisticated.
+	for (long e = 0; e < entities; e++)
+		if (position[e].en && position[e].x == x && position[e].y == y)
+			return e;
+	return -1;
 }
 
 
@@ -94,12 +132,17 @@ void draw_glyph(const char *glyph)
 	fflush(stdout);
 }
 
-void draw_thing(struct thing *t)
+void draw_entity(long e)
 {
-	if (!in_bounds(t->x, t->y))
+	if (!appearance[e].en || !position[e].en)
 		return;
-	cur_pos(t->x, t->y);
-	draw_glyph(t->glyph);
+
+	int x = position[e].x, y = position[e].y;
+	if (!in_bounds(x, y))
+		return;
+
+	cur_pos(x, y);
+	draw_glyph(appearance[e].glyph);
 }
 
 const char *ground_glyph(int x, int y)
@@ -113,13 +156,13 @@ void draw_loc(int x, int y)
 {
 	if (!in_bounds(x, y))
 		return;
-	cur_pos(x, y);
 
-	struct thing *t = thing_at(x, y);
-	if (t)
-		draw_glyph(t->glyph);
-	else
+	long e = entity_at(x, y);
+	cur_pos(x, y);
+	if (e < 0)
 		draw_glyph(ground_glyph(x, y));
+	else
+		draw_glyph(appearance[e].glyph);
 }
 
 void draw_all(void)
@@ -131,8 +174,63 @@ void draw_all(void)
 		printf("\r\n");
 	}
 
-	for (int i = 0; i < num_things; i++)
-		draw_thing(&things[i]);
+	for (long e = 0; e < entities; e++)
+		draw_entity(e);
+}
+
+
+/* ******** More Components ******** */
+
+// Motility
+struct {
+	bool en;
+	void (*style)(long self);
+	int tempo;
+	int timer;
+} motility[MAX_ENTITIES];
+
+void set_motility(long e, void (*style)(long self), int tempo)
+{
+	motility[e].en = true;
+	motility[e].style = style;
+	motility[e].tempo = tempo;
+	motility[e].timer = tempo;
+}
+
+void trigger_motility(void)
+{
+	for (long e = 0; e < entities; e++)
+		if (motility[e].en)
+			motility[e].style(e);
+}
+
+void move_entity(long e, unsigned char dir)
+{
+	if (!position[e].en)
+		return;
+
+	int dx = 0, dy = 0;
+	apply_dir(dir, &dx, &dy);
+
+	int x = position[e].x, y = position[e].y;
+	if ((dx || dy) && in_bounds(x + dx, y + dy)) {
+		position[e].x += dx;
+		position[e].y += dy;
+		draw_entity(e);
+		draw_loc(x, y);
+	}
+}
+
+void wander(long self)
+{
+	// FIXME: An entity with tempo k/2 does not always move at the same time as one at tempo k - why?
+	if (!motility[self].en)
+		return;
+	if (motility[self].timer --> 0)
+		return;
+
+	move_entity(self, '1' + rand() % 9); // TODO: Better RNG
+	motility[self].timer = motility[self].tempo;
 }
 
 
@@ -175,106 +273,71 @@ void install_signal_handlers(void)
 
 /* ******** Main ******** */
 
-unsigned long last_tick = 0;
-void tick()
+void trigger_all(void)
 {
-	last_tick = msec();
-
-	for (int i = 0; i < num_things; i++)
-		things[i].think(&things[i]);
+	// Trigger all components with runtime behavior
+	trigger_motility();
 }
 
-bool apply_dir(unsigned char dir, int *xp, int *yp)
+long make_inert(const char *glyph, int x, int y)
 {
-	static const int dx[UCHAR_MAX] = {
-		['1'] = -1, ['y'] = -1,
-		['4'] = -1, ['h'] = -1,
-		['7'] = -1, ['b'] = -1,
-		['3'] =  1, ['u'] =  1,
-		['6'] =  1, ['l'] =  1,
-		['9'] =  1, ['n'] =  1,
-	};
-	static const int dy[UCHAR_MAX] = {
-		['1'] =  1, ['b'] =  1,
-		['2'] =  1, ['j'] =  1,
-		['3'] =  1, ['n'] =  1,
-		['7'] = -1, ['y'] = -1,
-		['8'] = -1, ['k'] = -1,
-		['9'] = -1, ['u'] = -1,
-	};
-
-	int x = *xp + dx[dir];
-	int y = *yp + dy[dir];
-
-	if (in_bounds(x, y)) {
-		*xp = x;
-		*yp = y;
-		return true;
-	} else {
-		return false;
-	}
+	long e = new_entity();
+	set_appearance(e, glyph);
+	set_position(e, x, y);
+	return e;
 }
 
-void move_thing(struct thing *t, unsigned char dir)
+long make_wanderer(const char *glyph, int x, int y, int tempo)
 {
-	int x = t->x, y = t->y;
-	if (apply_dir(dir, &t->x, &t->y)) {
-		draw_loc(x, y);
-		draw_thing(t);
-	}
-}
-
-void inert(struct thing *self)
-{
-	(void)self;
-}
-
-void wander(struct thing *self)
-{
-	move_thing(self, '1' + rand() % 9);
-	// ^ TODO: Remove rand()
+	long e = make_inert(glyph, x, y);
+	set_motility(e, wander, tempo);
+	return e;
 }
 
 int main()
 {
-	install_signal_handlers();
-	screen_init();
-
-	struct thing *player = make_thing("\033[0;34m@", WIDTH / 2, HEIGHT / 2, inert);
-	make_thing("\033[0;31m@",     WIDTH / 3,     HEIGHT / 3, wander);
-	make_thing("\033[0;31m@",     WIDTH / 3, 2 * HEIGHT / 3, wander);
-	make_thing("\033[0;31m@", 2 * WIDTH / 3,     HEIGHT / 3, wander);
-	make_thing("\033[0;31m@", 2 * WIDTH / 3, 2 * HEIGHT / 3, wander);
-
-
 	struct pollfd in;
 	in.fd = fileno(stdin);
 	in.events = POLLIN;
 
+	long player = make_inert("\033[0;34m@", WIDTH / 2, HEIGHT / 2);
+	make_wanderer("\033[0;31m@",     WIDTH / 3,     HEIGHT / 3, 4);
+	make_wanderer("\033[0;31m@",     WIDTH / 3, 2 * HEIGHT / 3, 8);
+	make_wanderer("\033[0;31m@", 2 * WIDTH / 3,     HEIGHT / 3, 16);
+	make_wanderer("\033[0;31m@", 2 * WIDTH / 3, 2 * HEIGHT / 3, 32);
+
+	install_signal_handlers();
+	screen_init();
 	draw_all();
-	unsigned int reflex = 256;
+
+	unsigned long reflex = 16;
+	unsigned long next_tick = msec() + reflex;
 	while (!quit) {
 		int input = 0;
-		if (poll(&in, 1, reflex) > 0)
+		unsigned long timeout = next_tick - msec();
+		if (poll(&in, 1, timeout < 1 ? 1 : timeout) > 0)
 			input = getchar();
 
 		switch (input) {
 		case 0:
 			break;
-		case 4:
+		case 4: // Fallthrough
 		case 'q':
 			quit = true;
 			break;
 		default:
-			move_thing(player, input);
+			move_entity(player, input);
 			break;
 		}
 
 		unsigned long now = msec();
-		if (now - last_tick > reflex)
-			tick();
+		if (now > next_tick) {
+			next_tick = now + reflex;
+			trigger_all();
+		}
 	}
 
 	die();
+	// Unreachable
 	return 0;
 }
