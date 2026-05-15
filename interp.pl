e912c32bd3f76d77cccfa7eab695b6717131afb1
mkicjn <mkicjn@gmail.com>
2026-05-14 19:49:20 -0500

    Update names/comments

--- interp.pl
+++ interp.pl
@@ -7,152 +7,152 @@
 
 % Typing rules
 
-lb_env_type(_, true, bool).
-lb_env_type(_, false, bool).
+lc_env_type(_, true, bool).
+lc_env_type(_, false, bool).
 
-lb_env_type(Env, not(X), bool) :-
-	lb_env_type(Env, X, bool).
+lc_env_type(Env, not(X), bool) :-
+	lc_env_type(Env, X, bool).
 
-lb_env_type(Env, if(A,B,C), T) :-
-	lb_env_type(Env, A, bool),
-	lb_env_type(Env, B, T),
-	lb_env_type(Env, C, T).
+lc_env_type(Env, if(A,B,C), T) :-
+	lc_env_type(Env, A, bool),
+	lc_env_type(Env, B, T),
+	lc_env_type(Env, C, T).
 
-lb_env_type(_, 0, nat).
-lb_env_type(Env, succ(N), nat) :-
-	lb_env_type(Env, N, nat).
+lc_env_type(_, 0, nat).
+lc_env_type(Env, succ(N), nat) :-
+	lc_env_type(Env, N, nat).
 
-lb_env_type(Env, pred(N), nat) :-
-	lb_env_type(Env, N, nat).
+lc_env_type(Env, pred(N), nat) :-
+	lc_env_type(Env, N, nat).
 
-lb_env_type(Env, iszero(N), bool) :-
-	lb_env_type(Env, N, nat).
+lc_env_type(Env, iszero(N), bool) :-
+	lc_env_type(Env, N, nat).
 
-lb_env_type(Env, lambda(Var,Body), (TFrom->TTo)) :-
-	lb_env_type([Var-TFrom|Env], Body, TTo).
+lc_env_type(Env, lambda(Var,Body), (TFrom->TTo)) :-
+	lc_env_type([Var-TFrom|Env], Body, TTo).
 
-lb_env_type(Env, apply(Lambda,Arg), TTo) :-
-	lb_env_type(Env, Arg, TFrom),
-	lb_env_type(Env, Lambda, (TFrom->TTo)).
+lc_env_type(Env, apply(Lambda,Arg), TTo) :-
+	lc_env_type(Env, Arg, TFrom),
+	lc_env_type(Env, Lambda, (TFrom->TTo)).
 
-lb_env_type(Env, var(Var), T) :-
+lc_env_type(Env, var(Var), T) :-
 	member(Var-T, Env).
 
-lb_type(X, T) :-
-	lb_env_type([], X, T).
+lc_type(X, T) :-
+	lc_env_type([], X, T).
 
 
 % Helper relations
 
-lb_not(true, false).
-lb_not(false, true).
+lc_not(true, false).
+lc_not(false, true).
 
-lb_if(Env, if(true,X,_), R) :-
-	lb_env_eval(Env, X, R).
-lb_if(Env, if(false,_,X), R) :-
-	lb_env_eval(Env, X, R).
+lc_if(Env, if(true,X,_), R) :-
+	lc_env_eval(Env, X, R).
+lc_if(Env, if(false,_,X), R) :-
+	lc_env_eval(Env, X, R).
 
-lb_pred(0, 0).
-lb_pred(succ(N), N).
+lc_pred(0, 0).
+lc_pred(succ(N), N).
 
-lb_iszero(0, true).
-lb_iszero(succ(_), false).
+lc_iszero(0, true).
+lc_iszero(succ(_), false).
 
-lb_apply(closure(Env,Arg,Body), Val, R) :-
-	lb_env_eval([Arg-Val|Env], Body, R).
+lc_apply(closure(Env,Arg,Body), Val, R) :-
+	lc_env_eval([Arg-Val|Env], Body, R).
 
 
 % Interpreter
 % TODO: How to make type-directed?
 
-lb_env_eval(_, true, true).
+lc_env_eval(_, true, true).
 
-lb_env_eval(_, false, false).
+lc_env_eval(_, false, false).
 
-lb_env_eval(Env, not(X), R) :-
-	lb_env_eval(Env, X, Y),
-	lb_not(Y, R).
+lc_env_eval(Env, not(X), R) :-
+	lc_env_eval(Env, X, Y),
+	lc_not(Y, R).
 
-lb_env_eval(Env, if(Cond,Then,Else), R) :-
-	lb_env_eval(Env, Cond, C),
-	lb_if(Env, if(C,Then,Else), R).
+lc_env_eval(Env, if(Cond,Then,Else), R) :-
+	lc_env_eval(Env, Cond, C),
+	lc_if(Env, if(C,Then,Else), R).
 
-lb_env_eval(_, 0, 0).
+lc_env_eval(_, 0, 0).
 
-lb_env_eval(Env, succ(X), succ(R)) :-
-	lb_env_eval(Env, X, R).
+lc_env_eval(Env, succ(X), succ(R)) :-
+	lc_env_eval(Env, X, R).
 
-lb_env_eval(Env, pred(X), R) :-
-	lb_env_eval(Env, X, N),
-	lb_pred(N, R).
+lc_env_eval(Env, pred(X), R) :-
+	lc_env_eval(Env, X, N),
+	lc_pred(N, R).
 
-lb_env_eval(Env, iszero(X), R) :-
-	lb_env_eval(Env, X, N),
-	lb_iszero(N, R).
+lc_env_eval(Env, iszero(X), R) :-
+	lc_env_eval(Env, X, N),
+	lc_iszero(N, R).
 
-lb_env_eval(Env, lambda(Var,Body), closure(Env,Var,Body)).
+lc_env_eval(Env, lambda(Var,Body), closure(Env,Var,Body)).
 
-lb_env_eval(Env, apply(X, Y), R) :-
-	lb_env_eval(Env, X, X1),
-	lb_env_eval(Env, Y, Y1),
-	lb_apply(X1, Y1, R).
+lc_env_eval(Env, apply(X, Y), R) :-
+	lc_env_eval(Env, X, X1),
+	lc_env_eval(Env, Y, Y1),
+	lc_apply(X1, Y1, R).
 
-lb_env_eval(Env, var(Var), R) :-
+lc_env_eval(Env, var(Var), R) :-
 	member(Var-R, Env).
 
-lb_eval(X, R) :-
-	lb_env_eval([], X, R).
+lc_eval(X, R) :-
+	lc_env_eval([], X, R).
 
 
 % Iterative deepening setup
 
-lb_serial(true) --> [true].
-lb_serial(false) --> [false].
-lb_serial(not(X)) --> [not], lb_serial(X).
-lb_serial(if(X,Y,Z)) --> [if], lb_serial(X), lb_serial(Y), lb_serial(Z).
-lb_serial(0) --> [0].
-lb_serial(succ(X)) --> [succ], lb_serial(X).
-lb_serial(pred(X)) --> [pred], lb_serial(X).
-lb_serial(iszero(X)) --> [iszero], lb_serial(X).
-lb_serial(lambda(X,Y)) --> [lambda], [X], lb_serial(Y).
-lb_serial(apply(X,Y)) --> [apply], lb_serial(X), lb_serial(Y).
-lb_serial(var(X)) --> [var, X].
+lc_serial(true) --> [true].
+lc_serial(false) --> [false].
+lc_serial(not(X)) --> [not], lc_serial(X).
+lc_serial(if(X,Y,Z)) --> [if], lc_serial(X), lc_serial(Y), lc_serial(Z).
+lc_serial(0) --> [0].
+lc_serial(succ(X)) --> [succ], lc_serial(X).
+lc_serial(pred(X)) --> [pred], lc_serial(X).
+lc_serial(iszero(X)) --> [iszero], lc_serial(X).
+lc_serial(lambda(X,Y)) --> [lambda], [X], lc_serial(Y).
+lc_serial(apply(X,Y)) --> [apply], lc_serial(X), lc_serial(Y).
+lc_serial(var(X)) --> [var, X].
 
-lb_term_size(X, N) :-
+lc_term_size(X, N) :-
 	length(S, N),
-	phrase(lb_serial(X), S).
+	phrase(lc_serial(X), S).
 
-lb_term(X) :-
-	lb_term_size(X, _).
+lc_term(X) :-
+	lc_term_size(X, _).
 
 
 % Examples:
 %
 % Enumerate all terms:
-% ?- lb_term(X).
+% ?- lc_term(X).
 
 % Enumerate all well-typed terms:
-% ?- lb_term(X), lb_type(X, _).
+% ?- lc_term(X), lc_type(X, _).
 
 % Synthesis of expressions that invert their argument:
-% ?- lb_term(F), lb_eval(apply(F,true), false), lb_eval(apply(F,false), true).
+% ?- lc_term(F), lc_eval(apply(F,true), false), lc_eval(apply(F,false), true).
 
 % More synthesis tasks (fill in the blank):
-% ?- lb_term(F), lb_type(F, (_->_)), F=lambda(a,if(var(a),_,_)), lb_eval(apply(F,true), false), lb_eval(apply(F,false), true).
-% ?- lb_term(F), F=lambda(_,apply(_,_)), lb_type(F, (_->bool)), lb_eval(apply(F, lambda(a,not(var(a)))), true).
+% ?- lc_term(F), lc_type(F, (_->_)), F=lambda(a,if(var(a),_,_)), lc_eval(apply(F,true), false), lc_eval(apply(F,false), true).
+% ?- lc_term(F), F=lambda(_,apply(_,_)), lc_type(F, (_->bool)), lc_eval(apply(F, lambda(a,not(var(a)))), true).
 
 % Verification task - can this function ever return true?
-% F=lambda(a,if(var(a),not(var(a)),var(a))), lb_eval(apply(F,X), true).
+% F=lambda(a,if(var(a),not(var(a)),var(a))), lc_eval(apply(F,X), true).
 % ^ TODO: Does not terminate - how can it be made to?
 %   Obvious solution is:
-lb_bool(true).
-lb_bool(false).
-% ?- F=lambda(a,if(var(a),not(var(a)),var(a))), lb_bool(B), lb_eval(apply(F,B), true).
+lc_bool(true).
+lc_bool(false).
+% ?- F=lambda(a,if(var(a),not(var(a)),var(a))), lc_bool(B), lc_eval(apply(F,B), true).
 
 % Analogous relation for Nat:
-lb_nat(0).
-lb_nat(succ(X)) :-
-	lb_nat(X).
+lc_nat(0).
+lc_nat(succ(X)) :-
+	lc_nat(X).
 
 % Conversion to CLP(FD) integer:
 peano_num(0, 0).
@@ -162,7 +162,12 @@ peano_num(succ(P), N) :-
 	peano_num(P, N1).
 
 % Verification task - can this function ever return true?
-% ?- F=lambda(x, iszero(succ(var(x)))), lb_eval(F, C), lb_apply(C, N, true).
+% ?- F=lambda(x, iszero(succ(var(x)))), lc_eval(F, C), lc_apply(C, N, true).
+%    ^ Interesting note: Trying to correct the evaluation behavior of succ or pred,
+%      (i.e., by requiring their argument to be a ground Nat) yields non-termination here.
 
 % Evaluation task - recursive `iseven` function:
-% ?- F=lambda(x, if(iszero(var(x)), true, if(iszero(pred(var(x))), false, apply(F, pred(pred(var(x))))))), lb_eval(F, C), lb_apply(C, P, false), peano_num(P, N).
+% ?- F=lambda(x, if(iszero(var(x)), true, if(iszero(pred(var(x))), false, apply(F, pred(pred(var(x))))))), lc_eval(F, C), lc_apply(C, P, false), peano_num(P, N).
+
+% Metatheory task - find terms that are not well-typed, but do evaluate:
+% ?- lc_term(X), \+ lc_type(X, _), lc_eval(X, R).
